d85cc61e5aa714417579d3b3da9d91c223139562
Marcos <47260061+marcosazevedo2112@users.noreply.github.com>
2026-08-10 15:02:32 -0300

    feat(sales): register settlement and charge relations

--- src/modules/sales/sales/entities/associations.ts
+++ src/modules/sales/sales/entities/associations.ts
@@ -1,23 +1,14 @@
 import Sale from './sale.model';
 import SaleItem from './sale-item.model';
 import Installment from './installment.model';
+import Settlement from './settlement.model';
+import Charge from './charge.model';
 
-Sale.hasMany(SaleItem, {
-  foreignKey: 'saleId',
-  as: 'items',
-});
-
-SaleItem.belongsTo(Sale, {
-  foreignKey: 'saleId',
-  as: 'sale',
-});
-
-Sale.hasMany(Installment, {
-  foreignKey: 'saleId',
-  as: 'installments',
-});
-
-Installment.belongsTo(Sale, {
-  foreignKey: 'saleId',
-  as: 'sale',
-});
+Sale.hasMany(SaleItem, {foreignKey: 'saleId', as: 'items'});
+SaleItem.belongsTo(Sale, {foreignKey: 'saleId', as: 'sale'});
+Sale.hasMany(Installment, {foreignKey: 'saleId', as: 'installments'});
+Installment.belongsTo(Sale, {foreignKey: 'saleId', as: 'sale'});
+Sale.hasOne(Settlement, {foreignKey: 'saleId', as: 'settlement'});
+Settlement.belongsTo(Sale, {foreignKey: 'saleId', as: 'sale'});
+Installment.hasMany(Charge, {foreignKey: 'installmentId', as: 'charges'});
+Charge.belongsTo(Installment, {foreignKey: 'installmentId', as: 'installment'});
