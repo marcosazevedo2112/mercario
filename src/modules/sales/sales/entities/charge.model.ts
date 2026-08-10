05781f8a6ae60c5039368724ed095698b377d9e2
Marcos <47260061+marcosazevedo2112@users.noreply.github.com>
2026-08-10 15:00:56 -0300

    feat(sales): add charge aggregate

---
+++ src/modules/sales/sales/entities/charge.model.ts
@@ -0,0 +1,37 @@
+import {DataTypes, Model} from 'sequelize';
+import sequelize from '../../../../database/connection';
+
+import {ChargeChannel} from '../enums/charge-channel';
+import {ChargeTrigger} from '../enums/charge-trigger';
+
+class Charge extends Model {
+  declare id: number;
+  declare installmentId: number;
+  declare tenantId: number;
+  declare sentBy: number | 'system';
+  declare triggeredBy: ChargeTrigger;
+  declare channel: ChargeChannel;
+  declare message: string;
+  declare readonly createdAt: Date;
+}
+
+Charge.init(
+  {
+    id: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true},
+    installmentId: {type: DataTypes.INTEGER, allowNull: false},
+    tenantId: {type: DataTypes.INTEGER, allowNull: false},
+    sentBy: {type: DataTypes.STRING(20), allowNull: false},
+    triggeredBy: {type: DataTypes.ENUM(...Object.values(ChargeTrigger)), allowNull: false},
+    channel: {type: DataTypes.ENUM(...Object.values(ChargeChannel)), allowNull: false},
+    message: {type: DataTypes.TEXT, allowNull: false},
+  },
+  {
+    sequelize,
+    tableName: 'charges',
+    timestamps: true,
+    updatedAt: false,
+    indexes: [{fields: ['installmentId']}, {fields: ['tenantId']}],
+  },
+);
+
+export default Charge;
