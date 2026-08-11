3c82a80339b3d773703b0fb83bcc015fe415d3b1
Marcos <47260061+marcosazevedo2112@users.noreply.github.com>
2026-08-10 23:09:21 -0300

    feat(web): mount application page routes

--- src/routes/pages/index.ts
+++ src/routes/pages/index.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express';
 import authRoutes from './auth.routes';
+import appRoutes from './app.routes';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
+router.use('/', appRoutes);
 
 export default router;
