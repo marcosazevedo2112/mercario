ded909590292fa273e3febf110eadcb4edc0a6c2
Marcos <47260061+marcosazevedo2112@users.noreply.github.com>
2026-08-17 09:58:03 -0300

    feat(pages): register charge routes

--- src/routes/pages/index.ts
+++ src/routes/pages/index.ts
@@ -4,6 +4,7 @@ import homeRoutes from './home.routes';
 import customersRoutes from './customers.routes';
 import productsRoutes from './products.routes';
 import salesRoutes from './sales.routes';
+import chargesRoutes from './charges.routes';
 
 const router = Router();
 router.use(authRoutes);
@@ -11,4 +12,5 @@ router.use(homeRoutes);
 router.use(customersRoutes);
 router.use(productsRoutes);
 router.use(salesRoutes);
+router.use(chargesRoutes);
 export default router;
