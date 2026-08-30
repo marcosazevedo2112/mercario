a2856ee340fb0d69d33442b20b8b229d4d29d706
Marcos <47260061+marcosazevedo2112@users.noreply.github.com>
2026-08-29 22:08:54 -0300

    feat(sales): add SSR sale detail route

--- src/routes/pages/sales.routes.ts
+++ src/routes/pages/sales.routes.ts
@@ -5,4 +5,5 @@ import {authMiddleware} from '../../modules/shared/middlewares/authMiddleware';
 const router = Router();
 router.use(authMiddleware);
 router.get('/sales', SalesController.pageList);
+router.get('/sales/:id', SalesController.findById);
 export default router;
