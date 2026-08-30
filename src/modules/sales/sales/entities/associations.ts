a0c170d5169646e72932af2a070afde822e6bfb5
Marcos <47260061+marcosazevedo2112@users.noreply.github.com>
2026-08-29 22:07:45 -0300

    feat(sales): add sale-customer associations

--- src/modules/sales/sales/entities/associations.ts
+++ src/modules/sales/sales/entities/associations.ts
@@ -3,6 +3,7 @@ import SaleItem from './sale-item.model';
 import Installment from './installment.model';
 import Settlement from './settlement.model';
 import Charge from './charge.model';
+import Customer from '../../customers/customer.model';
 
 Sale.hasMany(SaleItem, {foreignKey: 'saleId', as: 'items'});
 SaleItem.belongsTo(Sale, {foreignKey: 'saleId', as: 'sale'});
@@ -12,3 +13,5 @@ Sale.hasOne(Settlement, {foreignKey: 'saleId', as: 'settlement'});
 Settlement.belongsTo(Sale, {foreignKey: 'saleId', as: 'sale'});
 Installment.hasMany(Charge, {foreignKey: 'installmentId', as: 'charges'});
 Charge.belongsTo(Installment, {foreignKey: 'installmentId', as: 'installment'});
+Sale.belongsTo(Customer, {foreignKey: 'customerId', as: 'customer'});
+Customer.hasMany(Sale, {foreignKey: 'customerId', as: 'sales'});
