5c9356d6e84039ed929495222599102000370686
Marcos <47260061+marcosazevedo2112@users.noreply.github.com>
2026-08-29 22:10:50 -0300

    feat(sales): add cancellation confirmation page route

--- src/routes/pages/sales.routes.ts
+++ src/routes/pages/sales.routes.ts
@@ -1,9 +1,9 @@
 import {Router} from 'express';
 import SalesController from '../../modules/sales/sale.controller';
 import {authMiddleware} from '../../modules/shared/middlewares/authMiddleware';
-
 const router = Router();
 router.use(authMiddleware);
 router.get('/sales', SalesController.pageList);
+router.get('/sales/:id/cancel', SalesController.pageCancel);
 router.get('/sales/:id', SalesController.findById);
 export default router;
